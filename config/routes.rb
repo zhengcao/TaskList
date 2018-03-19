3d4d86c4edb0aaaadb34969fc0268c1c4e824ded
Zheng Cao <chori84@gmail.com>
2018-03-19 15:12:34 -0700

    Update routes for Tasks controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,12 @@
 Rails.application.routes.draw do
-  get 'tasks/index'
 
-  get 'tasks/show'
-
-  get 'tasks/new'
-
-  get 'tasks/create'
-
-  get 'tasks/edit'
-
-  get 'tasks/update'
-
-  get 'tasks/destroy'
+  get '/tasks', to: 'tasks#index', as: 'tasks'
+  get '/tasks/:id', to: 'tasks#show', as: 'task'
+  get '/tasks/new', to: 'tasks#new', as: 'new_task'
+  post '/tasks', to: 'tasks#create'
+  get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  patch '/tasks/:id', to: 'tasks#update'
+  delete '/tasks/:id', to: 'tasks#destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
