8c38e76284c22bd1b84c16ea111b12cb45cbfe0d
Zheng Cao <chori84@gmail.com>
2018-03-22 20:48:02 -0700

    Finalize final content on views

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,25 +4,23 @@ class TasksController < ApplicationController
     @tasks = Task.all
   end
 
-  def show
-    id = params[:id]
-    @task = Task.find(id)
-  end
-
   def new
     @task = Task.new
   end
 
   def create
-    task = Task.new
-    task.title = params[:task][:title]
-    task.description = params[:task][:description]
-    task.completion_date = params[:task][:completion_date]
-    if task.save
+    @task = Task.new
+    @task.title = params[:task][:title]
+    @task.description = params[:task][:description]
+    if @task.save
       redirect_to tasks_path
     else
       render :new
     end
+  end
+
+  def show
+    @task = Task.find_by(id: params[:id])
   end
 
   def edit
@@ -32,9 +30,11 @@ class TasksController < ApplicationController
   def update
     @task = Task.find_by(id: params[:id])
 
-    if !@task.nil?
-      if @task.update(title: params[:task][:title], description: params[:task][:description], completion_date: params[:task][:completion_date])
+    if @task
+      @task.title = params[:task][:title]
+      @task.description = params[:task][:description]
 
+      if @task.save
         redirect_to task_path
       else
         render :edit
@@ -46,18 +46,26 @@ class TasksController < ApplicationController
     @task = Task.find_by(id: params[:id])
 
     if @task
-      @task.update(is_complete: true)
+      @task.update(is_complete: !@task.is_complete)
+
+      if @task.is_complete
+        @task.completion_date = @task.updated_at
+      else
+        @task.completion_date = nil
+      end
+      @task.save
     end
+
     redirect_to tasks_path
   end
 
   def destroy
-    id = params[:id]
-    @task = Task.find(id)
+    @task = Task.find_by(id: params[:id])
+
     if @task
       @task.destroy
     end
     redirect_to tasks_path
   end
-  
+
 end
